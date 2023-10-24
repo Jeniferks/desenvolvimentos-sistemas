3d7bc8ddfd4bc55e11113b99e0539af12b43aec2
jeniferkindermann <jeniferkindermann@gmail.com>
2023-10-23 22:01:39 -0300

    jogo completo

--- curso_senai/logica_de_programacao/numero-magico/script.js
+++ curso_senai/logica_de_programacao/numero-magico/script.js
@@ -10,7 +10,13 @@ let elementoinpPalpite = document.getElementById("inpPalpite")
 let elementoinpRestante = document.getElementById("inpRestante")
 let elementoinpPontos = document.getElementById("inpPontos")
 let elementoinpDicas = document.getElementById("inpDicas")
+let numeroSecreto 
+let maximoTentativas 
+let elementoInpMsg =  document.getElementById("msgJogador")
+let startButton = document.getElementById("guess__btn")
+let quitBtn = document.getElementById("quit__btn")
 
+//Pagina inicial
 function AiMode(){
     
     document.getElementById('select_div').style.display = 'none';
@@ -20,23 +26,73 @@ function AiMode(){
        
     }
     
+    
 }
+
+//Preparo de jogo
 
 function AiGameMode(){
+    maximoTentativas =  Number(elementoinpMax.value)
+    numeroSecreto = (Math.floor(Math.random() * Number(elementoinpMax.value) - Number(elementoinpMin.value) +1) + Number(elementoinpMin.value))
+    elementoinpRestante.value = elementoinpTentativas.value
+    console.log(numeroSecreto)
+    startButton.disabled = true
+    
 
-    let userNumber = document.getElementById("inpPve").value
-    let number = Number(Math.random()*10).toFixed()
-    let result = document.getElementById("resultadoFinal1")
-
-    if(userNumber == number){
-        result.innerHTML=`<p>Voce acertou</p>`
-        // document.getElementById('btn__return').style.display = 'flex';
-    }else{
-        result.innerHTML=`<p>Voce errou</p>`
-    }
-    console.log(number)
+    
 }
 
+//Começar palpites
+function userGuess(){
+    if((elementoinpRestante.value) > 0){
+        if( elementoinpPalpite.value == numeroSecreto  ){
+            elementoinpRestante.value
+            elementoinpDicas.value = elementoinpDicas.innerHTML = 'Você acertou'
+            elementoinpPontos.value = Number(elementoinpPontos.value) + 10
+            elementoInpMsg.value = elementoInpMsg.innerHTML = '<h2>Arrasou!Você Ganhou o jogo</h1>'
+    
+            
+        }else if(elementoinpPalpite.value > numeroSecreto){
+            elementoinpRestante.value = elementoinpRestante.value - 1
+            elementoinpPontos.value = elementoinpPontos.value - 10
+            elementoinpDicas.value = elementoinpDicas.innerHTML = 'Seu chute foi maior'
+            elementoInpMsg.value = elementoInpMsg.innerHTML = '<h2>Errou feio, errou rude</h2>'
+    
+            
+        }else{
+            elementoinpRestante.value = elementoinpRestante.value - 1
+            elementoinpPontos.value = elementoinpPontos.value - 10
+            elementoinpDicas.value = elementoinpDicas.innerHTML = 'Seu chute foi menor'
+            elementoInpMsg.value = elementoInpMsg.innerHTML = '<h2>Errou feio, errou rude</h2>'
+    
+        }
+    }else{
+        elementoInpMsg.value = elementoInpMsg.innerHTML = "<h3>Já era, perdeu playboy</h3>"
+        startButton.disabled = false
+        startButton.disabled = false
+        quitBtn.disabled = false
+    }
 
+}
 
+function quitGame(){
+    
+    elementoinpMin.value = ""
+    elementoinpMax.value = ""
+    elementoinpTentativas.value = ""
+    elementoinpPalpite.value = ""
+    elementoinpRestante.value = ""
+    elementoinpPontos.value = ""
+    elementoinpDicas.value = elementoinpDicas.innerHTML = ""
+    elementoInpMsg.value = elementoInpMsg.innerHTML = 'Recomece o jogo'
+    
+
+    if(elementoinpRestante.value <= 0){
+        startButton.disabled = false
+        quitBtn.disabled = true
+    }else{
+        startButton.disabled = true
+        quitBtn.disabled = false
+    }
+}
 
