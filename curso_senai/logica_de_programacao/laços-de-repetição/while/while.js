6a01d22b50781b637c220c43aefcddc2543bb5b5
jeniferkindermann <jeniferkindermann@gmail.com>
2023-11-09 20:28:24 -0300

    exemplos lucas

---
+++ curso_senai/logica_de_programacao/laços-de-repetição/while/while.js
@@ -0,0 +1,18 @@
+let numero = 0
+
+while (numero < 10){
+    
+    console.log("oi")
+    numero++
+}
+
+nome = ""
+
+while(nome != " Abigail"){
+    nome= prompt("Digite um nome: ")
+}
+
+
+for(cont=0; cont < 10; cont++){
+    alert(" oi")
+}
