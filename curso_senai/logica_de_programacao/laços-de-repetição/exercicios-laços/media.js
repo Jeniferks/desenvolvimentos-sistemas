73d989b6093a4ab8f33268ce2fbb27b69be9248c
jeniferkindermann <jeniferkindermann@gmail.com>
2023-11-09 21:54:45 -0300

    exercicio media utilizando FOR iniciado

---
+++ curso_senai/logica_de_programacao/laços-de-repetição/exercicios-laços/media.js
@@ -0,0 +1,19 @@
+/*Desenvolva um programa no qual o usuário deve digitar 
+o nome e a idade de 5 pessoas. Ao final mostrar a média
+de idade delas.*/
+
+let name
+let age
+let somaMedias 
+
+
+for (let index = 0; index < 5; index++) {
+    name = prompt(`Digite o nome da pessoa ${index + 1}:`);
+    age = Number(prompt(`Digite a idade de ${name} `));
+
+
+
+
+    
+}
+
