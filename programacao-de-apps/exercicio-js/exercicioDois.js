c45d9913d3c7d456c89e0f3aff462d48df98344d
jeniferkindermann <jeniferkindermann@gmail.com>
2024-03-05 16:53:15 -0300

    exercicio um

---
+++ programacao-de-apps/exercicio-js/exercicioDois.js
@@ -0,0 +1,11 @@
+function exercicioDois(){
+
+}
+
+// function mostrarHome(){
+//     document.getElementById('home').style.display = 'flex'
+//     document.getElementById('produtos').style.display = 'none'
+//     document.getElementById('servicos').style.display = 'none'
+//     document.getElementById('cadastro').style.display = 'none'
+//     document.getElementById('contatos').style.display = 'none'
+// }
---
+++ programacao-de-apps/exercicio-js/exercicioUm.js
@@ -0,0 +1,5 @@
+function exercicioUm() {
+        let nome = prompt("Digite seu Nome");
+        let idade = prompt("Digite sua idade");
+        console.log(`Olá meu nome é ${nome} e eu tenho ${idade} anos!`);
+}
